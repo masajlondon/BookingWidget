7c264b1786dd48134d9f442563a77a4e27b9b8b3
masajlondon <o.mcpheely@gmail.com>
2018-09-16 17:38:29 +0100

    fixing double bookings issue

--- dist/bookingv2.js
+++ dist/bookingv2.js
@@ -5554,8 +5554,8 @@ return /******/ (function(modules) { // webpackBootstrap
 				var args = {};
 		    if (getConfig().project_id) args.project_id = getConfig().project_id
 		    if (getConfig().resources) args.resources = getConfig().resources
-				args.from = eventData.starttime;
-				args.to = eventData.endtime;
+				args.between_timestamps.start = eventData.starttime;
+				args.between_timestamps.end = eventData.endtime;
 				console.log(eventData);
 
 				sdk
