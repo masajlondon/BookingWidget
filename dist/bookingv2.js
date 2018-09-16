e046ebecac46f8092bf1b2896b7b6e5beffe0e59
masajlondon <o.mcpheely@gmail.com>
2018-09-16 19:23:27 +0100

    fixing double bookings issue

--- dist/bookingv2.js
+++ dist/bookingv2.js
@@ -5572,7 +5572,7 @@ return /******/ (function(modules) { // webpackBootstrap
 				 var i;
 				 for (i = 0; i < response.data.length; i++) {
 					 if (response.data[i].start.substring(0, response.data[i].start.indexOf('+')) === eventData.start.format().substring(0, eventData.start.format().indexOf('+')) && response.data[i].end.substring(0, response.data[i].end.indexOf('+')) === eventData.end.format().substring(0, eventData.end.format().indexOf('+'))){
-						 console.log(eventData.start.format().substring(0, eventData.start.format().indexOf('+')))
+						 console.log(eventData.start.format().substring(0, eventData.start.format().indexOf('+')));
 						 return true;
 					 }
 				 }
